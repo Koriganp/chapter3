d121493d743598bc57ffff009f0c2bb00e8227fb
Korigan Payne <koriganp@gmail.com>
2017-09-26 15:22:58 -0600

    Introduction to PHP

--- test1.php
+++ test1.php
@@ -1,12 +1,10 @@
 <?php // test1.php
-  $username = "Korigan Payne";
-  echo $username;
-  echo "<br>";
-  $current_user = $username;
-  echo $current_user;
-  echo "<br>"
-?>
-<?php
-echo "This is line ".__LINE__."of file".__FILE__;
+	$username = "Korigan Payne";
+	echo $username;
+	echo "<br>";
+	$current_user = $username;
+	echo $current_user;
+	echo "<br>";
+	echo "This is line ".__LINE__." of file ".__FILE__;
 ?>
 
